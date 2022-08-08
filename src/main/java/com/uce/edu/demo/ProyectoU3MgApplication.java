b3e7326856d65fee18af0d3b7bec06212bc55b2a
Michael Garcia <msgr.2300290992@gmail.com>
2022-08-08 11:23:13 -0500

    Tarea 26: Tipos de JOIN INNER y OUTER en JPA

--- src/main/java/com/uce/edu/demo/ProyectoU3MgApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU3MgApplication.java
@@ -21,6 +21,9 @@ public class ProyectoU3MgApplication implements CommandLineRunner {
 
 	@Autowired
 	private IHotelService iHotelService;
+	
+	@Autowired
+	private IFacturaService iFacturaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU3MgApplication.class, args);
@@ -28,32 +31,60 @@ public class ProyectoU3MgApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		LOG.info("RELACIONAMIENTO WHERE");
-		List<Hotel> listaHoteles = this.iHotelService.buscarHotelJoinWhere("Familiar");
-		for (Hotel h : listaHoteles) {
-			LOG.info("Hotel: " + h.getNombre() + " " + h.getDireccion());
+		
+		List<Factura> listaFacturas = this.iFacturaService.buscarFacturaInnerJoin(3);
+		for (Factura factura : listaFacturas) {
+			LOG.info("Factura: " + " Numero: " + factura.getNumero() + " Fecha: " + factura.getFecha());
 		}
 
-		LOG.info("INNER JOIN EAGER/LAZY");
-		List<Hotel> listaHoteles2 = this.iHotelService.buscarHotelInnerJoin("Familiar");
-		for (Hotel h : listaHoteles2) {
-			LOG.info("Hotel2: " + h.getNombre() + " " + h.getDireccion());
-			for (Habitacion ha : h.getHabitaciones()) {
-				LOG.info("Habitacion2: " + ha);
-			}
-
+		List<Factura> listaFacturas2 = this.iFacturaService.buscarInnerJoin();
+		for (Factura factura : listaFacturas2) {
+			LOG.info("Factura2: " + " Numero: " + factura.getNumero() + " Fecha: " + factura.getFecha());
 		}
 
-		LOG.info("JOIN FETCH");
-		List<Hotel> listaHoteles3 = this.iHotelService.buscarHotelFetchJoin("Familiar");
-		for (Hotel h : listaHoteles3) {
-			LOG.info("Hotel3: " + h.getNombre() + " " + h.getDireccion());
-			for (Habitacion ha : h.getHabitaciones()) {
-				LOG.info("Habitacion3: " + ha);
-			}
-
+		// LEFT
+		List<Factura> listaFacturasLeft = this.iFacturaService.buscarFacturaOuterJoinLeft(3);
+		for (Factura factura : listaFacturasLeft) {
+			LOG.info("Factura: " + " Numero: " + factura.getNumero() + " Fecha: " + factura.getFecha());
 		}
+
+		// LEFT
+		List<Factura> listaFacturasLeft2 = this.iFacturaService.buscarOuterJoinLeft();
+		for (Factura factura : listaFacturasLeft2) {
+			LOG.info("Factura2: " + " Numero: " + factura.getNumero() + " Fecha: " + factura.getFecha());
+		}
+
+		// RIGHT
+		List<Factura> listaFacturasRight = this.iFacturaService.buscarFacturaOuterJoinRight(3);
+		for (Factura factura : listaFacturasRight) {
+			LOG.info("Factura: " + " Numero: " + factura.getNumero() + " Fecha: " + factura.getFecha());
+		}
+
+//		LOG.info("RELACIONAMIENTO WHERE");
+//		List<Hotel> listaHoteles = this.iHotelService.buscarHotelJoinWhere("Familiar");
+//		for (Hotel h : listaHoteles) {
+//			LOG.info("Hotel: " + h.getNombre() + " " + h.getDireccion());
+//		}
+//
+//		LOG.info("INNER JOIN EAGER/LAZY");
+//		List<Hotel> listaHoteles2 = this.iHotelService.buscarHotelInnerJoin("Familiar");
+//		for (Hotel h : listaHoteles2) {
+//			LOG.info("Hotel2: " + h.getNombre() + " " + h.getDireccion());
+//			for (Habitacion ha : h.getHabitaciones()) {
+//				LOG.info("Habitacion2: " + ha);
+//			}
+//
+//		}
+//
+//		LOG.info("JOIN FETCH");
+//		List<Hotel> listaHoteles3 = this.iHotelService.buscarHotelFetchJoin("Familiar");
+//		for (Hotel h : listaHoteles3) {
+//			LOG.info("Hotel3: " + h.getNombre() + " " + h.getDireccion());
+//			for (Habitacion ha : h.getHabitaciones()) {
+//				LOG.info("Habitacion3: " + ha);
+//			}
+//
+//		}
 
 	}
 
